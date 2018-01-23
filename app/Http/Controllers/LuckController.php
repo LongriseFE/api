c9437d30fc050b1752df4e1c6ceb404c6f778cdd
swimly <979741120@qq.com>
2018-01-23 14:54:10 +0800

    添加抽奖控制

--- app/Http/Controllers/LuckController.php
+++ app/Http/Controllers/LuckController.php
@@ -19,57 +19,70 @@ class LuckController extends Controller
         $pagesize = $request->pagesize;
         $prize = Prize::where('year', $year)->where('grade', $grade)->where('area', $area)->first();
         $selected = Luck::where('year', $year)->where('area', $area)->where('grade', $grade)->get();
-        if (!$prize) {
-            return json_encode(array(
-                'status'=>0,
-                'msg'=>'不存在该奖项设置！'
-            ));
-        } else if (count($selected) >= $prize->total) {
-            return json_encode(array(
-                'status'=>0,
-                'msg'=>'该奖项已经抽完！'
-            ));
-        } else if (intval($pagesize) > $prize->balance) {
-            return json_encode(array(
-                'status'=>0,
-                'msg'=>'抽奖人数超过奖项设置人数！'
-            ));
-        } else {
+        if(!intval($request->open)) {
             $users = array();
             if (Cache::get('users')) {
                 $users = Cache::get('users');
             } else {
                 $users = User::where('living', 'like', '%'.$area.'%')->get();
             }
-            $users = json_decode(json_encode($users), true);
-            $luck = array_rand($users, intval($pagesize));
-            $luckerInfo = array();
-            foreach ($luck as $k => $index) {
-                foreach($users as $key => $val) {
-                    if ($key === $index) {
-                        array_push($luckerInfo, $val);
-                        $lucker = new Luck();
-                        $lucker->uId = md5(uniqid());
-                        $lucker->ider = $val['uId'];
-                        $lucker->name = $val['name'];
-                        $lucker->face = $val['cover'];
-                        $lucker->sex = $val['sex'];
-                        $lucker->area = $area;
-                        $lucker->year = $year;
-                        $lucker->grade = $grade;
-                        $lucker->gift = $prize->gift;
-                        $lucker->save();
-                    }
-                }
-                unset($users[$index]);
-            }
-            $prize->balance = $prize->balance - $pagesize;
-            $prize->update();
-            Cache::set('users', $users, 10);
             return json_encode(array(
                 'length'=>count($users),
-                'luck'=>$luckerInfo
+                'luck'=>$users
             ));
+        } else {
+            if (!$prize) {
+                return json_encode(array(
+                    'status'=>0,
+                    'msg'=>'不存在该奖项设置！'
+                ));
+            } else if (count($selected) >= $prize->total) {
+                return json_encode(array(
+                    'status'=>0,
+                    'msg'=>'该奖项已经抽完！'
+                ));
+            } else if (intval($pagesize) > $prize->balance) {
+                return json_encode(array(
+                    'status'=>0,
+                    'msg'=>'抽奖人数超过奖项设置人数！'
+                ));
+            } else {
+                $users = array();
+                if (Cache::get('users')) {
+                    $users = Cache::get('users');
+                } else {
+                    $users = User::where('living', 'like', '%'.$area.'%')->get();
+                }
+                $users = json_decode(json_encode($users), true);
+                $luck = array_rand($users, intval($pagesize));
+                $luckerInfo = array();
+                foreach ($luck as $k => $index) {
+                    foreach($users as $key => $val) {
+                        if ($key === $index) {
+                            array_push($luckerInfo, $val);
+                            $lucker = new Luck();
+                            $lucker->uId = md5(uniqid());
+                            $lucker->ider = $val['uId'];
+                            $lucker->name = $val['name'];
+                            $lucker->face = $val['cover'];
+                            $lucker->sex = $val['sex'];
+                            $lucker->area = $area;
+                            $lucker->year = $year;
+                            $lucker->grade = $grade;
+                            $lucker->gift = $prize->gift;
+                            $lucker->save();
+                        }
+                    }
+                    unset($users[$index]);
+                }
+                $prize->balance = $prize->balance - $pagesize;
+                $prize->update();
+                Cache::set('users', $users, 10);
+                return json_encode(array(
+                    'length'=>count($users),
+                    'luck'=>$luckerInfo
+                ));
+            }
         }
     }
     public function reset (Request $request) {
